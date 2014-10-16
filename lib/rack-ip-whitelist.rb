2de935687946d012657b90c588c9588f26143a84
William Wedler <wwedler@riseup.net>
2014-10-15 20:26:57 -0400

    use a 403 document when in rails

--- lib/rack-ip-whitelist.rb
+++ lib/rack-ip-whitelist.rb
@@ -7,21 +7,22 @@ module Rack
     def initialize(app, addresses=nil)
       @app = app
       @ips = NetaddrList.parse addresses || ENV['WHITELISTED_IPS']
-      Rails.logger.info "[rack.ipwhitelist] whitelist: #{@ips.inspect}"
+      Rails.logger.info "[rack.ipwhitelist] whitelist: #{@ips.inspect}" if defined? Rails
     end
 
     def call(env)
       if white_listed?(env)
         @app.call(env)
       else
-        [ 401, {"Content-Type" => "text/html"}, ["<p>You are not authorized to view this site</p>"] ]
+        body = defined?(Rails) ?  File.open(File.join(Rails.root, 'public', '403.html'), 'r'): '<p>You are not authorized to view this site</p>'
+        [ 403, {"Content-Type" => "text/html"}, body]
       end
     end
     
     def white_listed?(env)
       address = env["HTTP_X_FORWARDED_FOR"] || env["REMOTE_ADDR"]
       allowed = @ips.contains? address
-      Rails.logger.info "[rack.ipwhitelist] access for remote ip #{address.inspect} #{allowed ? 'granted' : 'denied'}"
+      Rails.logger.info "[rack.ipwhitelist] access for remote ip #{address.inspect} #{allowed ? 'granted' : 'denied'}" if defined?(Rails)
       allowed
     end
 
